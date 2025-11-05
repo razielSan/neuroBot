0eae0908e51d9145bb4a9bd77d377c253998ec71
raziel <tvindzon@gmail.com>
2025-11-05 15:44:30 +0700

    create delete_data(app\utils\filesistem.py)

--- app/utils/filesistem.py
+++ app/utils/filesistem.py
@@ -1,5 +1,6 @@
-from typing import Tuple
+from typing import Tuple, Optional
 from pathlib import Path
+from logging import Logger
 
 
 def ensure_derictories(*args: Path) -> None:
@@ -11,3 +12,23 @@ def ensure_derictories(*args: Path) -> None:
 
     for dir in requiered_dirs:
         dir.mkdir(parents=True, exist_ok=True)
+
+
+def delete_data(
+    path: Path,
+    warning_logger: Optional[Logger] = None,
+) -> None:
+    """Удаляет данные по переданному пути
+
+    Args:
+        path (Path): Путь до данных
+        warning_logger (Optional[Logger], optional): логгер для записи в лог(По умолчанию None)
+    """
+    try:
+        if path.exists():
+            path.unlink()
+    except Exception as err:
+        message: str = f"Ошибка при удалении {path}: {err}"
+        if warning_logger:
+            warning_logger.exception(msg=message)
+        print(message)
