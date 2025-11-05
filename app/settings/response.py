51b90e89929364798ad8bf4448923bb8441df18f
raziel <tvindzon@gmail.com>
2025-11-05 15:43:17 +0700

    update app\settings\response.py

--- app/settings/response.py
+++ app/settings/response.py
@@ -1,9 +1,11 @@
 from core.config import Settings
+from core.message import DefaultMessages
 from utils.log_utils import setup_bot_logging
 
 
 settings: Settings = Settings()
 
+messages: DefaultMessages = DefaultMessages()
 
 # Создаем глобальные логгеры для всего приложения
 root_info_logger, root_warning_logger, root_error_logger = setup_bot_logging(
@@ -11,7 +13,5 @@ root_info_logger, root_warning_logger, root_error_logger = setup_bot_logging(
     base_path=settings.PATH_LOG_FOLDER,
     root_path=True,
     date_format=settings.DATE_FORMAT,
-    log_format=settings.LOG_FORMAT
+    log_format=settings.LOG_FORMAT,
 )
-
-
