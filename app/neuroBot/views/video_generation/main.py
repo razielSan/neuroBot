9ddadd3a0b3ebd38ebbf55d69e040bc22515d9b7
raziel <tvindzon@gmail.com>
2025-11-10 14:15:31 +0700

    update app\neuroBot\views\video_generation\main.py

--- app/neuroBot/views/video_generation/main.py
+++ app/neuroBot/views/video_generation/main.py
@@ -2,12 +2,21 @@ from aiogram.types import Message
 from aiogram import Router, F
 from aiogram.filters.state import StateFilter
 
-from neuroBot.extensions import bot_settings
+from neuroBot.extensions import (
+    bot_settings,
+    get_start_button_inline_menu_for_video_generation,
+)
 
 
 video_generation_router: Router = Router(name=bot_settings.BOT_ROUTER_NAME_3)
 
 
-@video_generation_router.message(StateFilter(None), F.text == "Генерация Видео")
+@video_generation_router.message(
+    StateFilter(None),
+    F.text == bot_settings.BOT_ROUTER_REPLY_KB_NAME_3,
+)
 async def image_description_handler(messsage: Message):
-    await messsage.answer("video_generation")
+    await messsage.answer(
+        "Доступные варианты",
+        reply_markup=get_start_button_inline_menu_for_video_generation,
+    )
