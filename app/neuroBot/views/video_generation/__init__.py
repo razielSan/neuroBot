4f586786fd26f0dcc6de862678b10937e6aa6c17
raziel <tvindzon@gmail.com>
2025-11-10 14:15:11 +0700

    update app\neuroBot\views\video_generation\__init__.py

--- app/neuroBot/views/video_generation/__init__.py
+++ app/neuroBot/views/video_generation/__init__.py
@@ -1,7 +1,10 @@
 from neuroBot.extensions import video_generation_error_logger, error_logger
 from neuroBot.middleware.errors_middleware import RouterErrorMiddleware
 from neuroBot.views.video_generation.main import video_generation_router
+from neuroBot.views.video_generation.vheer import vheer_router
 
+
+video_generation_router.include_router(vheer_router)
 
 video_generation_router.message.middleware(
     RouterErrorMiddleware(
